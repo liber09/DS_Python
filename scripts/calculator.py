19e0bf9f9f55cd44e72392eb658257f1d5b29bd8
liber09 <linda.bergsangel@hotmail.com>
2023-01-22 05:21:08 +0100

    Added comments

--- scripts/calculator.py
+++ scripts/calculator.py
@@ -3,33 +3,38 @@ from typing import Tuple
 global OPERATIONS
 OPERATIONS = ['+','-','*','/']
 
+#Add function
 def add(num1: int, num2: int):
     return num1+num2
 
+#subtract function
 def sub(num1: int, num2:int):
     return num1-num2
 
+#Multiplication function
 def multi(num1: int, num2: int):
     return num1*num2
 
+#division function
 def divide(num1: int, num2:int):
     while num2 == 0:
         print("Can not divide by zero")
         num1,num2 = get_input(prev_value= num1)
         return num1/num2
 
+#Get user input
 def get_input(prev_value: int | float | None) -> Tuple[int | float, int]:
     valid = False
     if prev_value:
         valid = True
         validated1 = prev_value
-    while not valid:
+    while not valid: #run as long as value is invalid
         choice1 = input("What is your first number?: ")
         valid = validate_input(choice1.strip())
         if valid:
             validated1 = int(choice1.strip())
     valid = False
-    while not valid:
+    while not valid: #As long as second number is invalid
         choice2 = input("What is your second number?: ")
         valid = validate_input(choice2.strip())
         if valid:
@@ -37,14 +42,14 @@ def get_input(prev_value: int | float | None) -> Tuple[int | float, int]:
 
     return (validated1, validated2)
 
-
+#Validate input value
 def validate_input(choice_string: str) -> bool:
     valid = str.isdigit(choice_string)
     if not valid:
         print("wrong input!!")
     return valid
 
-
+#What operation does the user want to do
 def get_operation() -> str:
     global OPERATIONS
     valid = False
@@ -67,6 +72,7 @@ def get_continue() -> bool:
             return False
 
 
+#Does the user want to use the answer for another calculation
 def get_re_use_result() -> bool:
     while True:
         choice = input("Do you want to reuse the result? ('y'/'n'): ")
--- scripts/hilow.py
+++ scripts/hilow.py
@@ -2,16 +2,18 @@
 
 import random
 
-max = 10
-min = 1
+max = 10 #max value
+min = 1 #min value
 
-score = 0
+score = 0 #current player score
 
+#The random number between min & max
 random_num = random.randint(min, max)
 
 while True:
     print("Your number is: ", random_num)
 
+    #Player guess
     guess = input("Let's guess, higher or lower? (h/l) (exit): ")
 
     if guess != "h" and guess != "l" and guess != "exit":
@@ -21,15 +23,19 @@ while True:
         print("Thank you for playing, better luck next time!")
         break
 
+    #new random number
     new_num = random.randint(min, max)
 
+    #Correct guess
     if new_num >= random_num and guess == "h":
         score += 1
         print("Wohooo!! Right, your current score is: ", score)
+    #Correct guess
     elif new_num <= random_num and guess == "l":
         score += 1
-        print("Wohooo!! Right, your current score is: ", score)
+        print("Wohooo!! Right, your current score is: ", score)    
     else:
+        #Wrong guess, reset score
         print("Ohhh noooo!!! That's wrong. Your score ended at: ", score)
         score = 0
         print("Try again")
